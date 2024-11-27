d261a0918f70eea74105dd81046bd85a852f0248
SarahAnberlin <sarahanberlin@outlook.com>
2024-11-27 11:17:18 +0800

    ?

--- robust_sam.py
+++ robust_sam.py
@@ -1,14 +1,8 @@
+import os
+
 from transformers import pipeline
 from PIL import Image
-
-# initialize the pipeline for mask generation
-generator = pipeline("mask-generation", model="jadechoghari/robustsam-vit-huge", device=0, points_per_batch=256)
-
-image_url = r'C:\Users\smartmore\Desktop\Test100\input\1.jpg'
-outputs = generator(image_url, points_per_batch=256)
-
 import matplotlib.pyplot as plt
-from PIL import Image
 import numpy as np
 
 
@@ -25,15 +19,37 @@ def show_mask(mask, ax, random_color=False):
     ax.imshow(mask_image)
 
 
-# display the original image
-plt.imshow(np.array(Image.open(image_url)))
-ax = plt.gca()
+# initialize the pipeline for mask generation
+generator = pipeline("mask-generation", model="jadechoghari/robustsam-vit-huge", device=0, points_per_batch=256)
 
-# loop through the masks and display each one
-for mask in outputs["masks"]:
-    show_mask(mask, ax=ax, random_color=True)
+data_root = '/dataset/vfayezzhang/test/SAM/data/Test100/input/'
+file_to_handle = []
 
-plt.axis("off")
+for root, dir, files in os.walk(data_root):
+    for file in files:
+        file_path = os.path.join(root, file)
+        if (file.endswith('.jpg') or file.endswith('.png')) and 'mask' not in file_path:
+            file_to_handle.append(file_path)
+for file_path in file_to_handle:
+    # load the image
+    image = Image.open(file_path)
 
-# show the image with the masks
-plt.show()
+    # generate the mask
+    outputs = generator(image, points_per_batch=256)
+
+    # display the original image
+    plt.imshow(np.array(image))
+    ax = plt.gca()
+
+    # loop through the masks and display each one
+    for mask in outputs["masks"]:
+        show_mask(mask, ax=ax, random_color=True)
+
+    plt.axis("off")
+
+    # show the image with the masks
+    plt.savefig(
+        os.path.join(data_root, f"{os.path.splitext(file)[0]}_robust_mask.png"),
+        bbox_inches='tight',  # 去掉多余的白边
+        pad_inches=0  # 不留填充空间
+    )
